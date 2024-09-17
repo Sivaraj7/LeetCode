dad3b3b0dc8c857f546c35beca45280950329960
Sivaraj7 <sr7112003@gmail.com>
2024-09-17 11:10:39 +0530

    Time: 89 ms (83.67%), Space: 49 MB (61.95%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,31 +1,27 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n < 2) return 0;
+  // Sieve of Eratosthenes algorithm to count the number of prime numbers less than a given integer n
 
-        
-        boolean[] isPrime = new boolean[n];
+  if (n < 2)
+  return 0;
+
+        boolean[] seen = new boolean[n];
         int ans = 0;
 
-       
-        for (int i = 2; i < n; i++) {
-            isPrime[i] = true;
-        }
-
-        for (int num = 2; num * num < n; num++) {
-            if (isPrime[num]) {
-                
-                for (int mult = num * num; mult < n; mult += num) {
-                    isPrime[mult] = false;
-                }
-            }
-        }
-
-        for (int i = 2; i < n; i++) {
-            if (isPrime[i]) {
-                ans++;
+        for (int num = 2; num < n; num++) 
+        {
+            if (seen[num]) 
+            continue;
+            ans++;
+            if ((long)num * num >= n) 
+            continue;
+           
+            for (int mult = num * num; mult < n; mult += num) 
+            {
+                seen[mult] = true;
             }
         }
 
         return ans;
-    }
+}
 }
